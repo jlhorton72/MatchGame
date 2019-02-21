2ac1d691cde97287595215454ff2138421868274
jh0375800 <jlhorton72@gmail.com>
2019-02-21 13:17:46 -0600

    First commit of Listing 16_2

---
+++ Listing16_2/src/ButtonDemo.java
@@ -0,0 +1,40 @@
+/*
+ * Name:        James Horton
+ * Date:        02/21/2019
+ * Program:     button demo
+ * Purpose:     demo some butons
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author jh0375800
+ */
+public class ButtonDemo extends Application{
+    @Override
+    public void start (Stage primaryStage){
+        
+    } // end of start
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args); 
+    } // end of main
+    
+} // end of class Button demo
